677c522e512eb091b306629324a97f2a916a31d8
AlexandraRuzina <Alexandra.Ruzina@Student.HTW-Berlin.de>
2026-06-24 12:27:15 +0200

    split

--- pipelines/mlops_pipeline.py
+++ pipelines/mlops_pipeline.py
@@ -5,7 +5,8 @@ from steps import (
     data_validation_gate,
     preprocess_data,
     feature_engineering,
-    data_post_processing_gate
+    data_post_processing_gate,
+    train_test_split
 )
 
 @pipeline
@@ -15,3 +16,4 @@ def mlops_pipeline():
     preprocess_df = preprocess_data.preprocess_data(validated_df)
     feature_engineering_df = feature_engineering.feature_engineering(preprocess_df)
     post_validated_df = data_post_processing_gate.data_post_processing_gate(feature_engineering_df)
+    train_test_split.train_test_split(post_validated_df)
---
+++ steps/train_test_split.py
@@ -0,0 +1,35 @@
+from zenml import step
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+
+@step
+def train_test_split_step(
+    df: pd.DataFrame,
+):
+    """
+    Teilt den Datensatz in Trainings- und Testdaten (80:20) auf.
+    Die Klassenverteilung der Zielvariable bleibt erhalten.
+    """
+
+    target_column = "class"
+
+    # Features und Zielvariable trennen
+    X = df.drop(columns=[target_column])
+    y = df[target_column]
+
+    # 80:20 Split
+    #random_state=42 Split ist reproduzierbar, bei jedem Pipeline -Lauf selbe Aufteilung
+    #Klassenverteilung bleibt erhalten --> stratify=y
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size=0.2,
+        random_state=42,
+        stratify=y
+    )
+
+    print(f"Trainingsdaten: {X_train.shape[0]} Zeilen")
+    print(f"Testdaten: {X_test.shape[0]} Zeilen")
+
+    return X_train, X_test, y_train, y_test
